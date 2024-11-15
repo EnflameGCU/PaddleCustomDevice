2458aff5d08d5416550b137c04816c4078e8bf53
EnflameGCU <118410644+EnflameGCU@users.noreply.github.com>
2024-11-15 17:26:21 +0800

    Revert "[GCU] Add dockerfile and some updates (#1459)"
    This reverts commit cc624af528b49461e29995fbc2b46b2686cc00a7.

--- backends/gcu/cmake/external/gcu.cmake
+++ backends/gcu/cmake/external/gcu.cmake
@@ -44,7 +44,7 @@ set(GCU_LIBS
     ${TOPSATENOP_LIB}
     -Wl,--as-needed)
 
-set(VERSION_REGEX "( [0-9]+\.[0-9]+\.[0-9]+\.(RC)?[0-9]*) ")
+set(VERSION_REGEX "( [0-9]+\.[0-9]+\.(RC)?[0-9]*) ")
 macro(find_gcu_version component)
   execute_process(
     COMMAND dpkg -l | grep ${component}
@@ -53,13 +53,7 @@ macro(find_gcu_version component)
     RESULT_VARIABLE VERSION_INFO_RESULT
     ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
   if(NOT ${SRC_VERSION_INFO})
-    message(STATUS "${component} SRC_VERSION_INFO is ${SRC_VERSION_INFO}")
-    set(VERSION_REGEX_FILTER
-        "(${component})( +[0-9]+\.[0-9]+\.[0-9]+\.(RC)?[0-9]*) ")
-    string(REGEX MATCH ${VERSION_REGEX_FILTER} VERSION_CONTENTS_FILTER
-                 "${SRC_VERSION_INFO}")
-    string(REGEX MATCH ${VERSION_REGEX} VERSION_CONTENTS
-                 "${VERSION_CONTENTS_FILTER}")
+    string(REGEX MATCH ${VERSION_REGEX} VERSION_CONTENTS "${SRC_VERSION_INFO}")
     string(STRIP "${VERSION_CONTENTS}" VERSION_STR)
     set(COMPONENT_VERSION ${VERSION_STR})
   else()
@@ -69,5 +63,5 @@ macro(find_gcu_version component)
   set(TOPS_VERSION ${COMPONENT_VERSION})
 endmacro()
 
-find_gcu_version("topsruntime")
+find_gcu_version("tops-sdk")
 message(STATUS "TOPS_VERSION is ${TOPS_VERSION}")
